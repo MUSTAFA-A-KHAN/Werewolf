a37d3824eca7fdcee6f24e838742142c77f7f2b0
Mustafa-a-khan <70553059+MUSTAFA-A-KHAN@users.noreply.github.com>
2026-06-24 16:59:10 +0000

    feat: fix player db

--- Werewolf for Telegram/Database/Player.cs
+++ Werewolf for Telegram/Database/Player.cs
@@ -11,10 +11,11 @@ namespace Database
 {
     using System;
     using System.Collections.Generic;
-    
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+
     public partial class Player
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public Player()
         {
             this.GameKills = new HashSet<GameKill>();
@@ -22,11 +23,13 @@ namespace Database
             this.GamePlayers = new HashSet<GamePlayer>();
             this.Groups = new HashSet<Group>();
         }
-    
+
+        [BsonId]
+        public ObjectId MongoId { get; set; }
+
         public int Id { get; set; }
         public long TelegramId { get; set; }
-        public string Name { get; set; }
-        public string UserName { get; set; }
+        public string Name { get; set; }        public string UserName { get; set; }
         public Nullable<bool> Banned { get; set; }
         public string BannedBy { get; set; }
         public Nullable<bool> HasPM { get; set; }
